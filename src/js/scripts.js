a1a3f7853b0d1d4784c1e7dd073999cdb72fb388
Jon Milner <email@jonmilner.com>
2016-06-03 15:51:23 -0400

    Initial Styling

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -7,6 +7,37 @@ var cq = require('cq-prolyfill')({
   postcss: true
 });
 
+function googleAnalytics() {
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-78740581-2', 'auto');
+  ga('send', 'pageview');
+}
+
+function twitterTimeline() {
+  window.twttr = (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0],
+      t = window.twttr || {};
+    if (d.getElementById(id)) return t;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "https://platform.twitter.com/widgets.js";
+    fjs.parentNode.insertBefore(js, fjs);
+
+    t._e = [];
+    t.ready = function(f) {
+      t._e.push(f);
+    };
+
+    return t;
+  }(document, "script", "twitter-wjs"));
+}
+
 $(document).ready(function() {
   removeOutline();
+  googleAnalytics();
+  twitterTimeline();
 });
